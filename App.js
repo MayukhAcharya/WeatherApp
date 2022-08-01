f71a8e35bb1bf8aa0a49f0186a8b6c99e59be9df
Max07092001 <mayukhacharya2001@gmail.com>
2022-08-02 01:14:18 +0530

    update 1.1

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   Image,
@@ -13,6 +13,8 @@ import axios from "axios";
 
 export default function App() {
   const [text, setText] = useState("");
+  const [temp, settemp] = useState([]);
+  const [feel, setFeel] = useState([]);
 
   //API Call
   const Weatherfetch = (text) => {
@@ -21,12 +23,20 @@ export default function App() {
       url: `https://api.openweathermap.org/data/2.5/weather?q=${text}&APPID=e372c354feb0d0a782d5e94cf94ff932`,
     })
       .then((response) => {
-        Alert.alert("response", JSON.stringify(response.data));
+        settemp(response.data.main.temp - 273);
+        setFeel(response.data.main.feels_like - 273);
+        //Alert.alert("response", JSON.stringify(response.data.main.temp - 273));
+        //area name,Temp,Feels like,etc etc.....in place of alert
       })
       .catch(function (error) {
         console.log("error", error);
       });
   };
+
+  //Applying useffect to optimize the api call
+  useEffect(() => {
+    Weatherfetch();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -44,6 +54,8 @@ export default function App() {
       />
       <View style={styles.txt1}>
         <Button title="Weather" onPress={() => Weatherfetch(text)} />
+        <Text>Temp = {temp}</Text>
+        <Text>Feels Like = {feel}</Text>
       </View>
 
       <StatusBar backgroundColor="white" barStyle="light-content" />
